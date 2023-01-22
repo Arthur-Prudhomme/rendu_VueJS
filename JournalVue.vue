fc2e617bad34d4a710dbcde9f1acfeaa650d3a12
Arthur Prud'homme <86523707+Arthur-Prudhomme@users.noreply.github.com>
2023-01-22 18:49:44 +0100

    :lipstick: css

---
+++ JournalVue.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="cartes">
+    <div v-for="call in callHist" :key="call.num">
+      <span>{{ call.name }} | {{ call.num }} | {{ call.date }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed:{
+        callHist(){
+            return this.$store.state.callHist
+        }
+    },
+}
+</script>
+<style scoped>
+  .cartes{
+    display:flex;
+    flex-direction: column;
+    overflow-y: scroll;
+  }
+</style>
